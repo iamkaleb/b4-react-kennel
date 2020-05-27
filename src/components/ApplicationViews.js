a61288e488cb1ad494db0fa22e550dd3b9a298ab
iamkaleb <kalebdmoran@gmail.com>
2020-05-27 14:54:41 -0500

    Loading data from API

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,11 +1,11 @@
 import { Route } from "react-router-dom";
 import React from "react";
 import Home from "./home/Home";
-import AnimalCard from "./animal/AnimalCard";
+import AnimalList from "./animal/AnimalList";
 //only include these once they are built - previous practice exercise
-import LocationCard from "./location/LocationCard";
-import EmployeeCard from "./employee/EmployeeCard";
-import OwnerCard from "./owner/OwnerCard";
+import LocationList from "./location/LocationList";
+import EmployeeList from "./employee/EmployeeList";
+import OwnerList from "./owner/OwnerList";
 
 const ApplicationViews = () => {
   return (
@@ -17,28 +17,28 @@ const ApplicationViews = () => {
           return <Home />;
         }}
       />
-      <Route
-        path="/animals"
-        render={props => {
-          return <AnimalCard />;
-        }}
+      <Route 
+        path="/animals" 
+        render={(props) => {
+        return <AnimalList />
+        }}  
       />
       <Route
         path="/locations"
         render={props => {
-          return <LocationCard />;
+          return <LocationList />;
         }}
       />
       <Route
         path="/owners"
         render={props => {
-          return <OwnerCard />;
+          return <OwnerList />;
         }}
       />
       <Route
         path="/employees"
         render={props => {
-          return <EmployeeCard />;
+          return <EmployeeList />;
         }}
       />
     </React.Fragment>
